13770db1f177c0495715db0b37b50659baeec501
castaneras <107968171+castaneras@users.noreply.github.com>
2022-12-19 19:59:49 -0500

    Namechange

--- src/Castaneras/LaravelFullcalendar/Event.php
+++ src/Castaneras/LaravelFullcalendar/Event.php
@@ -1,4 +1,4 @@
-<?php namespace MaddHatter\LaravelFullcalendar;
+<?php namespace Castaneras\LaravelFullcalendar;
 
 use DateTime;
 
--- src/Castaneras/LaravelFullcalendar/Facades/Calendar.php
+++ src/Castaneras/LaravelFullcalendar/Facades/Calendar.php
@@ -1,4 +1,4 @@
-<?php namespace MaddHatter\LaravelFullcalendar\Facades;
+<?php namespace Castaneras\LaravelFullcalendar\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
